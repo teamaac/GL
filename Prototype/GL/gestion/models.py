0cb4cfb2b33fae61ebe461add73f61a40fe3cacf
chedi <chedi.toueiti@gmail.com>
2012-06-26 03:35:57 +0100

    minor aestetic model update

--- Prototype/GL/gestion/models.py
+++ Prototype/GL/gestion/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class AbstractInfo(models.Model):
 	label       = models.CharField(max_length = 255, unique = True)
-	description = models.TextField()
+	description = models.TextField(                               )
 	class Meta           : abstract = True
 	def __unicode__(self): return self.label
 
@@ -12,53 +12,53 @@ class TypeComposant  (AbstractInfo):pass
 class VersionLogiciel(AbstractInfo):pass
 
 class Licence(models.Model):
-	label    = models.CharField(max_length = 255, unique = True)
-	gratuit  = models.BooleanField(default = False)
-	libre    = models.BooleanField(default = False)
-	contenu  = models.TextField()
+	label    = models.CharField   (max_length = 255, unique = True)
+	libre    = models.BooleanField(                               )
+	gratuit  = models.BooleanField(                               )
+	contenu  = models.TextField   (                               )
 	def __unicode__(self): return self.label
 
 class Composant(models.Model):
-	titre          = models.CharField(max_length = 255, unique = True)
+	titre          = models.CharField      (max_length = 255, unique = True              )
 	versions       = models.ManyToManyField(VersionLogiciel, through = 'ComposantVersion')
-	description    = models.TextField()
-	type_composant = models.ForeignKey(TypeComposant)
+	description    = models.TextField      (                                             )
+	type_composant = models.ForeignKey     (TypeComposant                                )
 	def __unicode__(self): return self.titre
 
 class ComposantVersion(models.Model):
-	composant      = models.ForeignKey(Composant)
+	cout           = models.FloatField(default = 0    )
+	nature         = models.ForeignKey(Nature         )
+	licence        = models.ForeignKey(Licence        )
 	version        = models.ForeignKey(VersionLogiciel)
-	nature         = models.ForeignKey(Nature)
-	licence        = models.ForeignKey(Licence)
-	cout           = models.FloatField(default = 0)
+	composant      = models.ForeignKey(Composant      )
 	class Meta           : unique_together = ('composant', 'version',)
 	def __unicode__(self): return "%s : %s" % (self.composant, self.version)
 
 class Client(models.Model):
-	designation = models.CharField(max_length = 255)
-	num_compte  = models.IntegerField()
-	description = models.TextField()
-	address     = models.TextField()
+	address        = models.TextField   (                )
+	designation    = models.CharField   (max_length = 255)
+	description    = models.TextField   (                )
+	numero_compte  = models.IntegerField(                )
 	def __unicode__(self): return self.designation
 
 class Produit(models.Model):
-	titre       = models.CharField(max_length = 255, unique = True)
+	titre       = models.CharField      (max_length = 255, unique = True            )
 	versions    = models.ManyToManyField(VersionLogiciel, through = 'ProduitVersion')
-	description = models.TextField()
+	description = models.TextField      (                                           )
 	def __unicode__(self): return self.titre
 
 class ProduitVersion(models.Model):
-	produit    = models.ForeignKey(Produit)
-	version    = models.ForeignKey(VersionLogiciel)
-	client     = models.ForeignKey(Client)
-	etat       = models.ForeignKey(Etat)
+	etat       = models.ForeignKey     (Etat                                                        )
+	client     = models.ForeignKey     (Client                                                      )
+	produit    = models.ForeignKey     (Produit                                                     )
+	version    = models.ForeignKey     (VersionLogiciel                                             )
 	composants = models.ManyToManyField(ComposantVersion, through = 'ProduitVersionComposantVersion')
 	class Meta            : unique_together = ('produit', 'version',)
 	def __unicode__ (self): return "%s : %s" % (self.produit, self.version)
 	def cout_nominal(self): return reduce(lambda x,y: x+y ,map(lambda x: x.cout, self.composants.all()), 0)
 
 class ProduitVersionComposantVersion(models.Model):
-	produit_version   = models.ForeignKey(ProduitVersion)
+	produit_version   = models.ForeignKey(ProduitVersion  )
 	composant_version = models.ForeignKey(ComposantVersion)
 	class Meta            : unique_together = ('produit_version', 'composant_version',)
 	def __unicode__(self) : return "%s -> %s" % (self.produit_version, self.composant_version)
