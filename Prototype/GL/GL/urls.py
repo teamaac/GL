f3126bb620b04cac7fa4a60a2c65b7b75bdd0ad5
chedi <chedi.toueiti@gmail.com>
2012-06-26 15:19:22 +0100

    adding new utls for tastypie tasty tools rest api

--- Prototype/GL/GL/urls.py
+++ Prototype/GL/GL/urls.py
@@ -1,12 +1,21 @@
+from gestion.api      import *
 from GL.settings      import *
+from tastypie.api     import Api
+from gestion.api      import *
 from django.contrib   import admin
 from django.conf.urls import patterns, include, url
 
 admin.autodiscover()
 
+gestion_api = Api(api_name='gestion')
+for resource in inspect.getmembers(gestion.api, inspect.isclass):
+	gestion_api.register(resource[1]())
+
 urlpatterns = patterns('',
 	url(r'^$'                        , 'gestion.views.index'),
+	url(r'^api/'                     , include(gestion_api.urls)),
 	url(r'^admin/products/report/$'  , 'gestion.administration.admin_views.product_chart_view'),
+	url(r'^tastytools/'              , include('tastytools.urls'), {'api_name': gestion_api.api_name}),
 	url(r'^admin/components/report/$', 'gestion.administration.admin_views.component_report'),
 	url(r'^admin/'                   , include(admin.site.urls                             )),
 	url(r'^admin_tools/'             , include('admin_tools.urls'                          )),
