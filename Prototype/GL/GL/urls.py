4d17482012f5f138ff5d73f240749f338e30392b
chedi <chedi.toueiti@gmail.com>
2012-06-26 03:37:33 +0100

    updating the produit and composant reports

--- Prototype/GL/GL/urls.py
+++ Prototype/GL/GL/urls.py
@@ -5,7 +5,8 @@ from django.conf.urls import patterns, include, url
 admin.autodiscover()
 
 urlpatterns = patterns('',
-	url(r'^admin/products/report/$'  , 'gestion.administration.admin_views.product_chart_view'  ),
+	url(r'^$'                        , 'gestion.views.index'),
+	url(r'^admin/products/report/$'  , 'gestion.administration.admin_views.product_chart_view'),
 	url(r'^admin/components/report/$', 'gestion.administration.admin_views.component_report'),
 	url(r'^admin/'                   , include(admin.site.urls                             )),
 	url(r'^admin_tools/'             , include('admin_tools.urls'                          )),
