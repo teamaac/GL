a5d33489595bea1e77d221d4562225a96a6de906
chedi <chedi.toueiti@gmail.com>
2012-06-28 14:48:50 +0100

    misc inspectation tools

---
+++ Prototype/GL/gestion/tools.py
@@ -0,0 +1,23 @@
+import re
+import inspect
+
+from django.db.models.fields.related import ReverseSingleRelatedObjectDescriptor
+
+def is_abstract_model_predicate(obj):
+    return inspect.isclass(obj) and not obj._meta.abstract
+
+def is_foreign_key_predicate(obj):
+    return inspect.isdatadescriptor(obj) and isinstance(obj, ReverseSingleRelatedObjectDescriptor)
+
+def module_classes(module):
+    return inspect.getmembers(module, inspect.isclass)
+
+def nonabstract_models(models_module):
+    return inspect.getmembers(models_module, is_abstract_model_predicate)
+
+def foreignkey_fields(model):
+    return map(lambda x: (x[0],x[1].field.rel.to), inspect.getmembers(model, is_foreign_key_predicate))
+
+def template_tests(module):
+    template_regex = re.compile('^_tpl_.*')
+    return filter(template_regex.search, [x[0] for x in inspect.getmembers(module, inspect.ismethod)])
