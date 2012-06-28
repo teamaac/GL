6e97f03c450bd406e0c182c7e8319a0bd58d79dc
chedi <chedi.toueiti@gmail.com>
2012-06-28 14:49:23 +0100

    tabs to spaces

--- Prototype/GL/gestion/admin.py
+++ Prototype/GL/gestion/admin.py
@@ -2,31 +2,31 @@ from gestion.models import *
 from django.contrib import admin
 
 class ComposantVersionInline(admin.TabularInline):
-	extra = 0
-	model = ComposantVersion
+    extra = 0
+    model = ComposantVersion
 
 class ProduitVersionInline(admin.TabularInline):
-	extra = 0
-	model = ProduitVersion
+    extra = 0
+    model = ProduitVersion
 
 class ProduitVersionComposantVersionInline(admin.TabularInline):
-	extra = 0
-	model = ProduitVersionComposantVersion
+    extra = 0
+    model = ProduitVersionComposantVersion
 
-class ProduitAdmin         (admin.ModelAdmin): inlines = [ProduitVersionInline  ,]
-class ComposantAdmin       (admin.ModelAdmin): inlines = [ComposantVersionInline,]
+class ProduitAdmin  (admin.ModelAdmin): inlines = [ProduitVersionInline  ,]
+class ComposantAdmin(admin.ModelAdmin): inlines = [ComposantVersionInline,]
 
 class ProduitVersionAdmin  (admin.ModelAdmin):
-	search_fields = ['produit__titre']
-	list_filter   = ('produit', 'version', 'client', 'etat')
-	list_display  = ('produit', 'version', 'client', 'etat', 'cout_nominal')
-	inlines = [ProduitVersionComposantVersionInline,]
+    search_fields = ['produit__titre']
+    list_filter   = ('produit', 'version', 'client', 'etat')
+    list_display  = ('produit', 'version', 'client', 'etat', 'cout_nominal')
+    inlines = [ProduitVersionComposantVersionInline,]
 
 class ComposantVersionAdmin(admin.ModelAdmin):
-	search_fields = ['composants__titre']
-	list_filter   = ('composant', 'version', 'nature', 'licence', 'cout')
-	list_display  = ('composant', 'version', 'nature', 'licence', 'cout')
-	inlines = [ProduitVersionComposantVersionInline,]
+    search_fields = ['composants__titre']
+    list_filter   = ('composant', 'version', 'nature', 'licence', 'cout')
+    list_display  = ('composant', 'version', 'nature', 'licence', 'cout')
+    inlines = [ProduitVersionComposantVersionInline,]
 
 admin.site.register( Etat                                   )
 admin.site.register( Nature                                 )
--- Prototype/GL/gestion/administration/dashboard.py
+++ Prototype/GL/gestion/administration/dashboard.py
@@ -4,61 +4,61 @@ from admin_tools.utils        import get_admin_site_name
 from admin_tools.dashboard    import modules, Dashboard, AppIndexDashboard
 
 class CustomIndexDashboard(Dashboard):
-	def init_with_context(self, context):
-		site_name = get_admin_site_name(context)
-		self.children.append(modules.LinkList(
-			_('Quick links'),
-			layout     = 'inline',
-			draggable  = False   ,
-			deletable  = False   ,
-			collapsible= False   ,
-			children=[
-				[_('Log out'), reverse('%s:logout' % site_name)],
-				[_('Change password'), reverse('%s:password_change' % site_name)],
-		]))
+    def init_with_context(self, context):
+        site_name = get_admin_site_name(context)
+        self.children.append(modules.LinkList(
+            _('Quick links'),
+            layout     = 'inline',
+            draggable  = False   ,
+            deletable  = False   ,
+            collapsible= False   ,
+            children=[
+                [_('Log out'), reverse('%s:logout' % site_name)],
+                [_('Change password'), reverse('%s:password_change' % site_name)],
+        ]))
 
-		self.children.append(modules.AppList(_('Applications'  ), exclude=('django.contrib.*',),))
-		self.children.append(modules.AppList(_('Administration'), models =('django.contrib.*',),))
-		self.children.append(modules.RecentActions(_('Recent Actions'), 10))
+        self.children.append(modules.AppList(_('Applications'  ), exclude=('django.contrib.*',),))
+        self.children.append(modules.AppList(_('Administration'), models =('django.contrib.*',),))
+        self.children.append(modules.RecentActions(_('Recent Actions'), 10))
 
-		self.children.append(modules.LinkList(
-			_('Support'),
-			children=[{
-					'title': _('Django documentation'),
-					'url': 'http://docs.djangoproject.com/',
-					'external': True,
-				},{
-					'title': _('Django admin-tools doumentation'),
-					'url': 'http://django-admin-tools.readthedocs.org/en/latest/index.html',
-					'external': True,
-				},{
-					'title': _('Extending Django Admin Interface'),
-					'url': 'http://www.djangobook.com/en/1.0/chapter17/',
-					'external': True,
-				},{
-					'title': _('Test-Driven Django Tutorial'),
-					'url': 'http://www.tdd-django-tutorial.com/',
-					'external': True,
-				},{
-					'title': _('Selenium documentation'),
-					'url': 'http://seleniumhq.org/docs/',
-					'external': True,
-				},
-			]
-		))
+        self.children.append(modules.LinkList(
+            _('Support'),
+            children=[{
+                    'title': _('Django documentation'),
+                    'url': 'http://docs.djangoproject.com/',
+                    'external': True,
+                },{
+                    'title': _('Django admin-tools doumentation'),
+                    'url': 'http://django-admin-tools.readthedocs.org/en/latest/index.html',
+                    'external': True,
+                },{
+                    'title': _('Extending Django Admin Interface'),
+                    'url': 'http://www.djangobook.com/en/1.0/chapter17/',
+                    'external': True,
+                },{
+                    'title': _('Test-Driven Django Tutorial'),
+                    'url': 'http://www.tdd-django-tutorial.com/',
+                    'external': True,
+                },{
+                    'title': _('Selenium documentation'),
+                    'url': 'http://seleniumhq.org/docs/',
+                    'external': True,
+                },
+            ]
+        ))
 
 
 class CustomAppIndexDashboard(AppIndexDashboard):
-	title = ''
-	def __init__(self, *args, **kwargs):
-		AppIndexDashboard.__init__(self, *args, **kwargs)
-		self.children += [
-			modules.ModelList(self.app_title, self.models),
-			modules.RecentActions(
-				_('Recent Actions'),
-				include_list=self.get_app_content_types(),
-				limit=5
-		)]
+    title = ''
+    def __init__(self, *args, **kwargs):
+        AppIndexDashboard.__init__(self, *args, **kwargs)
+        self.children += [
+            modules.ModelList(self.app_title, self.models),
+            modules.RecentActions(
+                _('Recent Actions'),
+                include_list=self.get_app_content_types(),
+                limit=5
+        )]
 
-	def init_with_context(self, context):
-		return super(CustomAppIndexDashboard, self).init_with_context(context)
+    def init_with_context(self, context):
+        return super(CustomAppIndexDashboard, self).init_with_context(context)
