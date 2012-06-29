0295735a18718ddd688cc8648b6179a2e7f0e859
chedi <chedi.toueiti@gmail.com>
2012-06-29 17:18:27 +0100

    refactoring of the resource module, now generated via the generate resource command

--- Prototype/GL/gestion/api/resources.py
+++ Prototype/GL/gestion/api/resources.py
@@ -1,35 +1,109 @@
-import sys
-import inspect
 import tastypie
 import settings
-import collections
 import tastypie.api
-import gestion.tools
 import tastypie.resources
+import gestion.models
 
-def create_resources(models_module=settings.MODELS_MODULE):
-    data          = collections.namedtuple('data'        , ['models_module', 'resources_map'  ])
-    model_fields  = collections.namedtuple('model_fields', ['field'        , 'mapped_resource'])
-    resources_map = collections.namedtuple('resource_map', [
-        'model_name'     ,
-        'full_reverse'   ,
-        'resource_name'  ,
-        'resource_class' ,
-        'fk_model_fields',
-        'mm_model_fields',])
-    result = data(models_module = models_module.__name__, resources_map = [])
-    for model in gestion.tools.nonabstract_models(models_module):
-        tmp_fk_resource_fields = []
-        tmp_mm_resource_fields = []
-        for field in gestion.tools.foreignkey_fields(model[1]):
-            tmp_fk_resource_fields.append(model_fields(field=field[0], mapped_resource=field[1].__name__+'Resource'))
-        for field in gestion.tools.many_to_many_fields(model[1]):
-            tmp_mm_resource_fields.append(model_fields(field=field[0], mapped_resource=field[1].__name__+'Resource'))
-        result.resources_map.append(resources_map(
-            model_name      = model[1].__name__,
-            full_reverse    = True,
-            resource_name   = model[1].__name__.lower(),
-            resource_class  = model[1].__name__+'Resource',
-            fk_model_fields = tmp_fk_resource_fields,
-            mm_model_fields = tmp_mm_resource_fields))
-    return result
+class ClientResource(tastypie.resources.ModelResource):
+    def determine_format(self, request):
+        return "application/json"
+    class Meta:
+        queryset      = gestion.models.Client.objects.all()
+        resource_name = 'client'
+
+class ComposantResource(tastypie.resources.ModelResource):
+    type_composant = tastypie.fields.ForeignKey('gestion.api.TypeComposantResource', 'type_composant', full=True)
+    versions = tastypie.fields.ToManyField('gestion.api.VersionLogicielResource', 'versions', full=True)
+    def determine_format(self, request):
+        return "application/json"
+    class Meta:
+        queryset      = gestion.models.Composant.objects.all()
+        resource_name = 'composant'
+
+class ComposantVersionResource(tastypie.resources.ModelResource):
+    composant = tastypie.fields.ForeignKey('gestion.api.ComposantResource', 'composant', full=True)
+    licence = tastypie.fields.ForeignKey('gestion.api.LicenceResource', 'licence', full=True)
+    nature = tastypie.fields.ForeignKey('gestion.api.NatureResource', 'nature', full=True)
+    version = tastypie.fields.ForeignKey('gestion.api.VersionLogicielResource', 'version', full=True)
+    def determine_format(self, request):
+        return "application/json"
+    class Meta:
+        queryset      = gestion.models.ComposantVersion.objects.all()
+        resource_name = 'composantversion'
+
+class EtatResource(tastypie.resources.ModelResource):
+    def determine_format(self, request):
+        return "application/json"
+    class Meta:
+        queryset      = gestion.models.Etat.objects.all()
+        resource_name = 'etat'
+
+class LicenceResource(tastypie.resources.ModelResource):
+    def determine_format(self, request):
+        return "application/json"
+    class Meta:
+        queryset      = gestion.models.Licence.objects.all()
+        resource_name = 'licence'
+
+class NatureResource(tastypie.resources.ModelResource):
+    def determine_format(self, request):
+        return "application/json"
+    class Meta:
+        queryset      = gestion.models.Nature.objects.all()
+        resource_name = 'nature'
+
+class ProduitResource(tastypie.resources.ModelResource):
+    versions = tastypie.fields.ToManyField('gestion.api.VersionLogicielResource', 'versions', full=True)
+    def determine_format(self, request):
+        return "application/json"
+    class Meta:
+        queryset      = gestion.models.Produit.objects.all()
+        resource_name = 'produit'
+
+class ProduitVersionResource(tastypie.resources.ModelResource):
+    client = tastypie.fields.ForeignKey('gestion.api.ClientResource', 'client', full=True)
+    etat = tastypie.fields.ForeignKey('gestion.api.EtatResource', 'etat', full=True)
+    produit = tastypie.fields.ForeignKey('gestion.api.ProduitResource', 'produit', full=True)
+    version = tastypie.fields.ForeignKey('gestion.api.VersionLogicielResource', 'version', full=True)
+    composants = tastypie.fields.ToManyField('gestion.api.ComposantVersionResource', 'composants', full=True)
+    def determine_format(self, request):
+        return "application/json"
+    class Meta:
+        queryset      = gestion.models.ProduitVersion.objects.all()
+        resource_name = 'produitversion'
+
+class ProduitVersionComposantVersionResource(tastypie.resources.ModelResource):
+    composant_version = tastypie.fields.ForeignKey('gestion.api.ComposantVersionResource', 'composant_version', full=True)
+    produit_version = tastypie.fields.ForeignKey('gestion.api.ProduitVersionResource', 'produit_version', full=True)
+    def determine_format(self, request):
+        return "application/json"
+    class Meta:
+        queryset      = gestion.models.ProduitVersionComposantVersion.objects.all()
+        resource_name = 'produitversioncomposantversion'
+
+class TypeComposantResource(tastypie.resources.ModelResource):
+    def determine_format(self, request):
+        return "application/json"
+    class Meta:
+        queryset      = gestion.models.TypeComposant.objects.all()
+        resource_name = 'typecomposant'
+
+class VersionLogicielResource(tastypie.resources.ModelResource):
+    def determine_format(self, request):
+        return "application/json"
+    class Meta:
+        queryset      = gestion.models.VersionLogiciel.objects.all()
+        resource_name = 'versionlogiciel'
+
+resources_api = tastypie.api.Api(api_name=settings.API_NAME)
+resources_api.register(ClientResource())
+resources_api.register(ComposantResource())
+resources_api.register(ComposantVersionResource())
+resources_api.register(EtatResource())
+resources_api.register(LicenceResource())
+resources_api.register(NatureResource())
+resources_api.register(ProduitResource())
+resources_api.register(ProduitVersionResource())
+resources_api.register(ProduitVersionComposantVersionResource())
+resources_api.register(TypeComposantResource())
+resources_api.register(VersionLogicielResource())
--- Prototype/GL/gestion/api/settings.py
+++ Prototype/GL/gestion/api/settings.py
@@ -1,7 +1,6 @@
 import sys
-import resources
 import gestion.models
 
-API_NAME         = "model"
-MODELS_MODULE    = gestion.models
-RESOURCES_MODULE = sys.modules[globals()['__name__']]
+API_NAME              = "model"
+MODELS_MODULE         = gestion.models
+RESOURCES_MODULE_NAME = sys.modules[globals()['__name__']].__package__
