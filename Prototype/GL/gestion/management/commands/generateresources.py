037dc3b209ca51131ad4bae6c1a59c0dab5fbde5
chedi <chedi.toueiti@gmail.com>
2012-06-29 15:15:59 +0100

    updated the command generateresource

--- Prototype/GL/gestion/management/commands/generateresources.py
+++ Prototype/GL/gestion/management/commands/generateresources.py
@@ -1,4 +1,4 @@
-from Cheetah.Template            import Template
+from jinja2 import Environment, PackageLoader
 from gestion.api.resources       import create_resources
 from django.core.management.base import BaseCommand, CommandError
 
@@ -6,5 +6,10 @@ class Command(BaseCommand):
     help = 'generate resource classes'
 
     def handle(self, *args, **options):
-        resources_map = create_resources()
-        
+        data           = create_resources()
+        environment    = Environment(loader=PackageLoader('gestion', 'management/commands'))
+        template       = environment.get_template('resources.tpl')
+        generated_data = template.render(
+            resources_map = data.resources_map,
+            models_module = data.models_module)
+        print generated_data
