d82a92afc153495947c9f4991079bd9adb070491
chedi <chedi.toueiti@gmail.com>
2012-06-29 17:18:54 +0100

    adding custom resource file generation to the command option

--- Prototype/GL/gestion/management/commands/generateresources.py
+++ Prototype/GL/gestion/management/commands/generateresources.py
@@ -1,5 +1,6 @@
-from jinja2 import Environment, PackageLoader
-from gestion.api.resources       import create_resources
+from sys                         import modules
+from jinja2                      import Environment, PackageLoader
+from gestion.api.tools           import create_resources
 from django.core.management.base import BaseCommand, CommandError
 
 class Command(BaseCommand):
@@ -7,9 +8,13 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         data           = create_resources()
+        path           = modules[data.api_module].__path__[0]
+        result_file    = open(path+'/resources.py', 'w')
         environment    = Environment(loader=PackageLoader('gestion', 'management/commands'))
         template       = environment.get_template('resources.tpl')
         generated_data = template.render(
+            api_module    = data.api_module,
             resources_map = data.resources_map,
-            models_module = data.models_module)
-        print generated_data
+            models_module = data.models_module,)
+        result_file.write(generated_data)
+        result_file.close()
